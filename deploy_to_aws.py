de388790ca0c78aed558e53e4b9c1d53f24fc415
André Sartori <afs25@cam.ac.uk>
2020-09-01 15:16:12 +0100

    #3533 comment: adapted deploy_to_aws.py to use imported class

--- deploy_to_aws.py
+++ deploy_to_aws.py
@@ -1,96 +1,12 @@
 #!/usr/bin/env python3
 
-import json
-import subprocess
-import sys
-import requests
+from thiscovery_dev_tools.deploy_to_aws import AwsDeployer
 
-import src.common.utilities as utils
-from local.dev_config import SECRETS_NAMESPACE
-from local.secrets import STACKERY_CREDENTIALS, SLACK_DEPLOYMENT_NOTIFIER_WEBHOOKS
-from src.common.dynamodb_utilities import STACK_NAME
-
-
-def slack_message(environment, branch, message=None):
-    if not message:
-        message = f"Branch {branch} of {STACK_NAME} has just been deployed to {environment}."
-    header = {
-        'Content-Type': 'application/json'
-    }
-    payload = {
-        "text": message
-    }
-    requests.post(SLACK_DEPLOYMENT_NOTIFIER_WEBHOOKS['stackery-deployments'], data=json.dumps(payload), headers=header)
-    if 'afs25' in environment:
-        requests.post(SLACK_DEPLOYMENT_NOTIFIER_WEBHOOKS['Andre'], data=json.dumps(payload), headers=header)
-
-
-def stackery_deployment(environment, branch):
-    profile = utils.namespace2profile(utils.name2namespace(environment))
-    try:
-        subprocess.run(['stackery', 'deploy', f'--stack-name={STACK_NAME}', f'--aws-profile={profile}',
-                        f'--env-name={environment}', f'--git-ref={branch}'], check=True,
-                       stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as err:
-        print(err.stderr.decode('utf-8').strip())
-        raise err
-
-
-def stackery_login():
-    try:
-        subprocess.run(['stackery', 'login', '--email', STACKERY_CREDENTIALS['email'],
-                        '--password', STACKERY_CREDENTIALS['password']],
-                       check=True, stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError as err:
-        print(err.stderr.decode('utf-8').strip())
-        raise err
-
-
-def get_git_branch():
-    branch = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, check=True,
-                            text=True).stdout.strip()
-    status = subprocess.run(['git', 'status'], capture_output=True, check=True, text=True).stdout.strip()
-    if ('Your branch is ahead' in status) or ('Changes not staged for commit' in status):
-        while True:
-            proceed = input('It looks like your local branch is out of sync with remote. Continue anyway? [y/N] (or "s" to show "git status")')
-            if proceed.lower() == 's':
-                print(status)
-                print("--------------------------")
-            elif proceed.lower() not in ['y', 'yes']:
-                sys.exit('Deployment aborted')
-            else:
-                break
-    return branch
-
-
-def deployment_confirmation(environment, branch):
-    proceed = input(f'About to deploy branch {branch} of {STACK_NAME} to {environment}. Continue? [y/N]')
-    if not proceed.lower() in ['y', 'yes']:
-        sys.exit('Deployment aborted')
-
-
-def deploy(environment, branch=None):
-    if branch is None:
-        branch = get_git_branch()
-    try:
-        stackery_deployment(environment, branch)
-    except subprocess.CalledProcessError as err:
-        if err.stderr.decode('utf-8').strip() == "Error: Failed to get settings: Attempting to access Stackery " \
-                                                 "before logging in. Please run `stackery login` first.":
-            stackery_login()
-            stackery_deployment(environment, branch)
-        else:
-            raise err
-
-
-def main(environment):
-    target_branch = get_git_branch()
-    deployment_confirmation(environment, target_branch)
-    deploy(environment, target_branch)
-    slack_message(environment, target_branch)
+import local.dev_config  # set environment variables
+import local.secrets  # set environment variables
+from src.endpoints import STACK_NAME
 
 
 if __name__ == '__main__':
-
-    main(environment=utils.namespace2name(SECRETS_NAMESPACE))
-
+    deployer = AwsDeployer(stack_name=STACK_NAME)
+    deployer.main()
